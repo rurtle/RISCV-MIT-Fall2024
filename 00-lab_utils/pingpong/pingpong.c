b102da29f01cfd287d5371163117112c28b43062
Subhra S. Sarkar <rurtle.coder@gmail.com>
2026-01-12 00:22:01 +0000

    Able to read from one end of the pipe now. Still need to fix the reading at parent end

--- 00-lab_utils/pingpong/pingpong.c
+++ 00-lab_utils/pingpong/pingpong.c
@@ -5,7 +5,20 @@
 int
 main(int argc, char *argv[])
 {
-	fprintf(2, "Inside ping-pong...\n");
+	// int fdc[2];
+	int fds[2];
+	char x;
+
+	// Sanity check
+	if (pipe(fds) < 0) {
+		printf("pingpong: pipe failed\n");
+		exit(1);
+	}
+
+	// Parent sending a byte to the child
+	if (write(fds[1], "Z", 1) != 1)
+		printf("pingpong: pipe write failed at parent - 1\n");
+
 
 	// const char c = 'w';			// This is the byte I want to send back-and-forth
 	int pid = fork();
@@ -13,13 +26,23 @@ main(int argc, char *argv[])
 		printf("fork failed\n");
 		exit(1);
 	} else if (pid == 0) {
-		// Inside child process
-		printf("Inside child\n");
+		if (read(fds[0], &x, 1) != 1) {
+			printf("pingpong: pipe read failed\n");
+		} else {
+			printf("%d: received ping: %d\n", getpid(), x);
+			if (write(fds[1], "A", 1) != 1) {
+				printf("pingpong: write failed: child\n");
+			}
+		}
 
 	} else {
 		// Inside parent process
 		wait(0);
-		printf("Inside parent\n");
+		if (read(fds[0], &x, 1) != 0) {
+			printf("pingpong: read failed: parent\n");
+		} else {
+			printf("%d: received pong: %d\n", getpid(), x);
+		}
 	}
 
 	// Graceful exit
