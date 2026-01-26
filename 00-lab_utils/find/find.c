67de9993509c79c8ed627e446193e298af05be89
Subhra S. Sarkar <rurtle.coder@gmail.com>
2026-01-26 11:00:21 +0000

    Added stubs for validating the path and stat. Next, we'll have to recurse through the directory structure

--- 00-lab_utils/find/find.c
+++ 00-lab_utils/find/find.c
@@ -1,6 +1,38 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+void find(char *path, char *filename)
+{
+	int fd;
+	// struct dirent de;
+	struct stat st;
+
+	if ((fd = open(path, O_RDONLY)) < 0) {
+		fprintf(2, "find: cannot open %s\n", path);
+		return;
+	}
+
+	if (fstat(fd, &st) < 0) {
+		fprintf(2, "find: cannot stat %s\n", path);
+		close(fd);
+		return;
+	}
+
+	switch(st.type) {
+		case T_DEVICE:
+		case T_FILE:
+			// printf("%d %d %d %d\n", fmtname(path), st.type, st.ino, (int) st.size);
+			printf("%d %d %d\n", st.type, st.ino, (int) st.size);
+			break;
+		case T_DIR:
+			printf("find: recurse through directory %s\n", path);
+			break;
+	}
+	close(fd);
+}
 
 int
 main(int argc, char *argv[])
@@ -10,6 +42,9 @@ main(int argc, char *argv[])
 		fprintf(2, "Usage: find <directory> <filename to find>...\n");
 		exit(1);
 	}
+
+	// Assuming we've received the correct set of arguments
+	find(argv[1], argv[2]);
 
 	// Graceful exit
 	exit(0);
